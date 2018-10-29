ad349488bc583bd6aeb463862d270adcee79e999
Malte Klaumann <mk@jochenlinnemann.de>
2018-10-29 13:49:04 +0100

    Added a dew more translations

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Versionskontrollzentrum
--------------------------
 A Chrome extensions that 'translates' specific Git terms to the German language.
 The thing is. It is always a very literal translation.
--- translate.js
+++ translate.js
@@ -12,9 +12,25 @@ for (var i = 0; i < elements.length; i++) {
             replacedText = replacedText.replace(/branches/gi,'Zweige');
             replacedText = replacedText.replace(/branch/gi,'Zweig');
             replacedText = replacedText.replace(/Branch/gi,'Zweig');
+            replacedText = replacedText.replace(/Pull requests/gi,'Zug Anfragen');
             replacedText = replacedText.replace(/New pull request/gi,'Neue Zug Anfrage');
             replacedText = replacedText.replace(/commits/gi,'Verpflichtungen');
+            replacedText = replacedText.replace(/commit/gi,'Verpflichtung');
             replacedText = replacedText.replace(/feature/gi,'Merkmal');
+            replacedText = replacedText.replace(/Issues/gi,'Probleme');
+            replacedText = replacedText.replace(/Projects/gi,'Vorhaben');
+            replacedText = replacedText.replace(/Marketplace/gi,'Marktplatz');
+            replacedText = replacedText.replace(/Explore/gi,'Erkunden');
+            replacedText = replacedText.replace(/Unwatch/gi,'Wegschauen');
+            replacedText = replacedText.replace(/Unwatch/gi,'Wegschauen');
+            replacedText = replacedText.replace(/Not Watching/gi,'Wegschauen');
+            replacedText = replacedText.replace(/Watching/gi,'Hinschauen');
+            replacedText = replacedText.replace(/Ignoring/gi,'ignorieren');
+            replacedText = replacedText.replace(/Watch/gi,'Hinschauen');
+            replacedText = replacedText.replace(/Unstar/gi,'Entsternen');
+            replacedText = replacedText.replace(/Star/gi,'Sternen');
+            replacedText = replacedText.replace(/Fork/gi,'Gabeln');
+            replacedText = replacedText.replace(/Merge pull request/gi,'Verschmelze Zuganfrage');
 
             if (replacedText !== text) {
                 element.replaceChild(document.createTextNode(replacedText), node);
